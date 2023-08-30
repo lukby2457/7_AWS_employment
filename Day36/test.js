561bdd225977fa12773da42af07e7f5831131af8
lukby2457 <139111692+lukby2457@users.noreply.github.com>
2023-08-30 15:16:41 +0900

    Add files via upload
    js / Day36 / js basic

---
+++ Day36/test.js
@@ -0,0 +1,13 @@
+// node.js 설치 후 실행
+// 1. ctrl + F5
+// 2. ctrl + j -> 터미널 오픈 -> node[파일명]
+console.log('hello node!!!\n');
+
+console.log(`True AND True : ${true && true}\n`);
+
+for (var i = 1; i <= 5; i++) {
+    console.log(`${i} : Hello World!!!`);
+}
+
+// exe -> : compiler(빠름, 오류 수정에 느림)
+// one line -> : interpreter(느림, 오류 수정에 빠름. Web에서 주로 사용됨)
